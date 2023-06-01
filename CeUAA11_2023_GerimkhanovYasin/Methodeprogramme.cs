d5abfd7a3938e07c7fa5b2249dfe1262e29a763f
Gerimkhanov Yasin <200460@CCM-Asty-Moulin.local>
2023-06-01 15:21:29 +0200

    examen UAA11

---
+++ CeUAA11_2023_GerimkhanovYasin/Methodeprogramme.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CeUAA11_2023_GerimkhanovYasin
+{
+    
+    public struct Methodeprogramme
+    {
+        public void CryptVigenere(string phClaire, string phClef, out string MatVigenere)
+        {
+            int codeAscii;
+            int j;
+            MatVigenere = new string[4, phClaire.Length];
+            j = 0;
+            for (int i = 0; i < phClaire.Length - 1; i++)
+            {
+                MatVigenere[0, i] = phClaire[i];
+                if (j == phClef.Length)
+                {
+                    j = 0;
+                }
+                else
+                {
+
+                }
+                MatVigenere[1, i] = phClef[j];
+                MatVigenere[2, i] = ((int)phClef[j] - 65).ToString();
+                if (((int)phClaire[i] + int.Parse(MatVigenere[2, i]) <= 90))
+                {
+                    codeAscii == (int)char.Parse(MatVigenere[0, i]) + int.Parse(MatVigenere[2, i]);
+                }
+                else
+                {
+                    codeAscii == (int)char.Parse(MatVigenere[0, i]) + int.Parse(MatVigenere[2, i]) - 26;
+                }
+                MatVigenere[3, i] = Convert.ToChar(codeAscii).ToString();
+                j = j + 1;
+            }
+
+
+        }
+        public void CryptAffine(string phClaire, int a, int b)
+        {
+            int x;
+            int y;
+            string[,] matAffine = new string[4, phClaire.Length];
+            for (int i = 0; i < phClaire.Length - 1; i++)
+            {
+                matAffine[0, 1] = phClaire[i];
+                x = ((int)phClaire[i] - 65);
+                matAffine[1, i] = x;
+                y = (a * x + b) % 26;
+                matAffine[2, i] = y;
+                matAffine[3, i] = y + 65;
+            }
+        }
+    }
+}
---
+++ CeUAA11_2023_GerimkhanovYasin/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CeUAA11_2023_GerimkhanovYasin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phClaire;
+            string choix;
+            string phClef;
+            string MatVigenere;
+            int a;
+            int b;
+            string matAffine;
+
+            Console.WriteLine("Menu : \n 1) Cryptage Vigenere  \n 2) Cryptage Affine \n ");
+            choix = Console.ReadLine();
+            Methodeprogramme mesOutils = new Methodeprogramme();
+            if (choix == "1")
+
+            {
+                Console.WriteLine("donner la phrase a crypter");
+                phClaire = Console.ReadLine();
+                Console.WriteLine("donner la clef a cryptage");
+                phClef = Console.ReadLine();
+
+                mesOutils.CryptVigenere(phClaire, phClef, out MatVigenere);
+            }
+            if (choix == "2")
+
+            {
+                Console.WriteLine("donner la phrase a crypter");
+                phClaire = Console.ReadLine();
+                Console.WriteLine("donner le nombre pour a");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("donner le nombre pour b");
+                b = int.Parse(Console.ReadLine());
+
+                mesOutils.CryptAffine(phClaire, a, b);
+
+            }
+
+
+        }
+    }
+}
